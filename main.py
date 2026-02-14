5b432da5d88aab19a469e33206b1b8e78f59da6a
Francis San Juan <francismigsanjuan@gmail.com>
2026-02-13 18:31:37 -0800

    Adds main interface, misplaced tile heuristic

--- main.py
+++ main.py
@@ -1,15 +1,85 @@
 # Original subroutines
 from puzzle import Puzzle
-from search import A_star, manhattan_dist
+from search import A_star, manhattan_dist, misplaced_tile
 
-def main():
-    p = Puzzle([1, 2, 3, 0, 4, 5, 7, 8, 6])
-    print(p)
-    A_star(p)
+def set_custom_puzzle() -> Puzzle:
+    PUZZLE_SIZE_MESSAGE = (
+                '\nHow big would you like your n x n puzzle to be? (integers >= 2 only!)\n'
+            )
+    print(PUZZLE_SIZE_MESSAGE)
+    size = int(input('n = '))
 
-    p = Puzzle([0, 2, 3, 1])
-    print(p)
-    A_star(p)
+    if size < 2:
+        raise ValueError('Custom puzzle size is less than minimum!')
+
+    PUZZLE_INPUT_INSTRUCTIONS = (
+                '\nYou will now be asked to input your puzzle row by row.\n'
+                f'For each row, please input {size} numbers separated by spaces.\n'
+                'Please represent the blank (empty) tile with a 0.\n'
+
+                '\nSimple 2x2 example:\n'
+                '1 0\n'
+                '3 2\n'
+            )
+    print(PUZZLE_INPUT_INSTRUCTIONS)
+    
+    puzzle_state = []
+    for i, row in enumerate(range(size)):
+        input_row = input(f'Row {i + 1}: ')
+        
+        row_state = []
+        for num in input_row.split(' '):
+            row_state.append(int(num))
+        
+        puzzle_state.extend(row_state)
+
+    return Puzzle(puzzle_state)
+
+# Main program
+def main() -> None:
+    WELCOME_MESSAGE = (
+                'Welcome to the n-puzzle solver!\n\n'
+                'To begin, please select an algorithm to solve with:\n'
+                '\t1) Uniform Cost Search\n'
+                '\t2) A* (Misplaced Tile Heuristic)\n'
+                '\t3) A* (Manhattan Distance Heuristic)\n'
+            )
+    print(WELCOME_MESSAGE)
+    
+    algo_map = {'1': None, '2': misplaced_tile, '3': manhattan_dist}
+    algorithm_choice = input('(1 / 2 / 3): ')
+
+    if algorithm_choice in algo_map:
+        algorithm = algo_map[algorithm_choice] 
+    
+    PUZZLE_INPUT_MESSAGE = (
+                '\nHow would you like to set the puzzle?:\n'
+                '\t1) I would like to enter my own n x n puzzle!\n'
+                '\t2) I would like to use a default 3x3 puzzle.\n'
+            )
+    print(PUZZLE_INPUT_MESSAGE)
+    puzzle_input_choice = input('(1 / 2): ')
+
+    # Depth 11 default 3x3 puzzle
+    puzzle = Puzzle([
+            2, 3, 5,
+            0, 8, 4,
+            1, 7, 6
+        ])
+
+    if puzzle_input_choice == '1':
+        puzzle = set_custom_puzzle()
+
+    SHOW_NODES_MESSAGE = (
+                '\nLastly, would you like to see node information for nodes visited by the search?\n'
+                '(This could generate a wall of text and increase the time taken by the algorithm!)\n'
+                '\t1) No, just solve it already!\n'
+                '\t2) Yes, show me the nodes!\n'
+            )
+    print(SHOW_NODES_MESSAGE)
+    show_nodes = True if input('(1 / 2): ') == '2' else False
+
+    A_star(puzzle, algorithm, show_nodes)
 
 if __name__ == '__main__':
     main()
--- search.py
+++ search.py
@@ -7,6 +7,7 @@ from puzzle import Puzzle, expand
 
 # Unoriginal subroutine
 from heapq import heappop, heappush
+from time import perf_counter_ns
 
 # Node class for search
 class Node():
@@ -38,8 +39,20 @@ def index_to_grid(index: int, size: int):
 
     return row, col
 
+# Misplaced tile heuristic
+def misplaced_tile(p: Puzzle) -> int:
+    goal = list(range(1, p.size**2)) + [0]
+    misplaced_count = 0
+
+    for current, true in zip(p.state, goal):
+        if current != 0 and current != true:
+            misplaced_count += 1
+
+    return misplaced_count
+
+
 # Manhattan distance heuristic
-def manhattan_dist(p: Puzzle):
+def manhattan_dist(p: Puzzle) -> int:
     total_dist = 0
 
     for i, tile in enumerate(p.state):
@@ -53,31 +66,61 @@ def manhattan_dist(p: Puzzle):
     return total_dist
 
 # A* implementation with custom heuristic as a parameter (If None, then its just Uniform Cost Search since h(x) = 0)
-def A_star(initial_puzzle: Puzzle, heuristic: Callable[Puzzle] = None):
+def A_star(initial_puzzle: Puzzle, heuristic: Callable[[Puzzle], int] | None = None, show_nodes: bool = False):
+    print('\nSOLVING...\n')
+    
+    # Timing for measurements
+    start_time = perf_counter_ns()
+
     # Establish goal state for a n x n puzzle
     goal = list(range(1, initial_puzzle.size**2)) + [0]
     
     # Create queue with initial state as the only node
     queue = [Node(0, 0, initial_puzzle)]
     
+    nodes_expanded = 0
+    max_queue_size = 0
+
+    visited = set()
+
     # Breaks when goal state is found or no solution is found
     while True:
         if len(queue) == 0:
-            print('FAILURE\n')
+            print('NO SOLUTION POSSIBLE')
             break
 
+        max_queue_size = max(len(queue), max_queue_size)
+
         node = heappop(queue)
+        state_str = "".join(map(str, node.puzzle.state))
+        
+        if state_str in visited:
+            continue
+        visited.add(state_str)
+
+        if show_nodes:
+            print(f'g(x): {node.depth} | h(x): {node.cost}')
+            print(node.puzzle)
+
         if node.puzzle.state == goal:
-            print('SUCCESS\n')
+            print('SOLUTION FOUND')
             break
 
         new_puzzles = expand(node.puzzle)
+        nodes_expanded += 1
 
         for puz in new_puzzles:
             # If no heuristic is used, h(x) = 0
             cost = 0 if heuristic == None else heuristic(puz)
             heappush(queue, Node(node.depth + 1, cost, puz))
-    
-    print(node.puzzle)
-    print(f'Final Node Depth: {node.depth}, Cost: {node.cost}')
 
+    end_time = perf_counter_ns()
+    
+    SUMMARY_MESSAGE = (
+                '\nSEARCH SUMMARY\n'
+                f'Highest Amount Of Nodes In Queue: {max_queue_size}\n'
+                f'Total Nodes Expanded: {nodes_expanded}\n'
+                f'Final Node Depth: {node.depth}\n'
+                f'Time Elapsed: {(end_time - start_time) / 1_000_000} ms'
+            )
+    print(SUMMARY_MESSAGE)
